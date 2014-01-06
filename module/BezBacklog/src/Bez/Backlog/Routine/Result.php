0d2ddd2637c2510b1de238def44954a3c9ce064e
Bez Hermoso <bezalelhermoso@gmail.com>
2014-01-05 23:25:52 -0800

    Added interfaces for routines and associated result class.

---
+++ module/BezBacklog/src/Bez/Backlog/Routine/Result.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright 2014 Bezalel Hermoso <bezalelhermoso@gmail.com>
+ * 
+ * This project is free software released under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php 
+ */
+
+namespace Bez\Backlog\Routine;
+
+
+use Bez\Backlog\Entity\JobInterface;
+
+class Result
+{
+    const SUCCESS = 1;
+
+    const FAILURE = 0;
+
+    const STOP = -1;
+
+    protected $status;
+
+    protected $job;
+
+    protected $messages;
+
+    public function __construct($status, JobInterface $job = null, $messages = null)
+    {
+        $this->status = $status;
+
+        if (null !== $job) {
+            $this->job = $job;
+        }
+
+        if (null !== $messages) {
+            $this->messages = is_array(messages) ? $messages : array($messages);
+        }
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function getJob()
+    {
+        return $this->job;
+    }
+} 
---
+++ module/BezBacklog/src/Bez/Backlog/Routine/RoutineInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright 2014 Bezalel Hermoso <bezalelhermoso@gmail.com>
+ * 
+ * This project is free software released under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php 
+ */
+
+namespace Bez\Backlog\Routine;
+
+
+use Bez\Backlog\Entity\JobInterface;
+
+interface RoutineInterface
+{
+    /**
+     * @param JobInterface $job
+     * @return Result
+     * @throws \Exception
+     */
+    public function process(JobInterface $job);
+} 
