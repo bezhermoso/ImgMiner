289367f519ae111ad9cbce71c26231361ef39cc4
Bez Hermoso <bezalelhermoso@gmail.com>
2014-01-06 06:46:11 -0800

    Config boilerplate.

--- module/BezBacklogModule/config/module.config.php
+++ module/BezBacklogModule/config/module.config.php
@@ -2,9 +2,12 @@
 
 return array(
     'backlog' => array(
+        'repository_manager' => 'Bez\Backlog\RepositoryManager\ServiceLocator',
         'repositories' => array(
             'foo' => 'Backlog\FooRepository',
         ),
+
+        'routine_manager' => 'Bez\Backlog\RoutineManager\ServiceLocator',
         'routines' => array(
             'foo' => 'Backlog\FooRoutine',
         ),
