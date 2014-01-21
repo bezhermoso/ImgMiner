c9aab0bb0262f654622ebf9c5a30551b8e00de9d
Bez Hermoso <bezalelhermoso@gmail.com>
2014-01-20 20:46:32 -0800

    Module file.

---
+++ module/ImgMiner/Module.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright 2014 Bezalel Hermoso <bezalelhermoso@gmail.com>
+ * 
+ * This project is free software released under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php 
+ */
+
+namespace ImgMiner;
+
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ControllerProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+
+class Module implements
+    ConfigProviderInterface,
+    ServiceProviderInterface,
+    ControllerProviderInterface
+{
+
+    /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/src/module.config.php';
+    }
+
+    /**
+     * Expected to return \Zend\ServiceManager\Config object or array to seed
+     * such an object.
+     *
+     * @return array|\Zend\ServiceManager\Config
+     */
+    public function getControllerConfig()
+    {
+        return include __DIR__ . '/src/module.controllers.php';
+    }
+
+    /**
+     * Expected to return \Zend\ServiceManager\Config object or array to
+     * seed such an object.
+     *
+     * @return array|\Zend\ServiceManager\Config
+     */
+    public function getServiceConfig()
+    {
+        return include __DIR__ . '/src/module.services.php';
+    }
+}
